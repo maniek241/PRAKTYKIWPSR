6bd75590636b6b0a3e36b086693156d5ff8a2046
maniek241 <manio2502i@wp.pl>
2020-12-09 15:02:28 +0100

    zadanko

--- src/kostka/main.java
+++ src/kostka/main.java
@@ -1,13 +1,6 @@
 package kostka;
     public class main {
         public static void main(String[] args){
-            //   public static void main(String[] args){
-
-            //       Deck deck= new Deck();
-
-            //     for (Card c: deck.deck){
-            //         System.out.println(c);
-            //      }
 
             Dice dice= new Dice(4);
 
